eb37e38555197eeaa39d5f092597f30f078430d9
Augustin Viard <gugusviard@gmail.com>
2025-10-10 14:15:53 +0200

    bug/fixing some responsive issues

--- components/sections/presentation.tsx
+++ components/sections/presentation.tsx
@@ -11,70 +11,76 @@ export const PresentationSection = () => {
         age = year - 2001;
     }
     return (
-        <section id="accueil" className="bg-[#b4cff8] dark:bg-[#1a1d33] flex flex-col h-[100vh] items-center justify-center">
-            <motion.h1
-                initial={{ opacity: 0, y: "-100vw" }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ type: "spring", stiffness: 30, delay: 0.2 }}
-                className="text-5xl lg:text-9xl pb-2 animate-[--animate-slide-in]"
-            >
-                Augustin Viard
-            </motion.h1>
-            <motion.h2
-                className="mb-[5vh] lg:mb-[10vh] text-3xl lg:text-5xl"
-                initial={{ opacity: 0, x: "100vw" }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ type: "spring", stiffness: 30, delay: 0.2 }}
-            >
-                Développeur Web - Full Stack
-            </motion.h2>
+        <section id="accueil" className="bg-[#b4cff8] dark:bg-[#1a1d33] flex flex-col h-screen items-center justify-between pt-[5vh] md:px-[5vh]">
+            <div className="flex flex-col justify-center items-center">
+                <motion.h1
+                    initial={{ opacity: 0, y: "-100vw" }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ type: "spring", stiffness: 30, delay: 0.2 }}
+                    className="text-5xl md:text-7xl lg:text-9xl pb-2 animate-[--animate-slide-in]"
+                >
+                    Augustin Viard
+                </motion.h1>
+                <motion.h2
+                    className="text-3xl md:text-4xl lg:text-5xl"
+                    initial={{ opacity: 0, x: "100vw" }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: "spring", stiffness: 30, delay: 0.2 }}
+                >
+                    Développeur Web - Full Stack
+                </motion.h2>
+            </div>
             <motion.p
-                className="text-center mx-3 text-xl lg:text-3xl"
+                className="text-center mx-3 text-xl md:text-2xl lg:text-3xl"
                 initial={{ opacity: 0, x: "100vw" }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ type: "spring", stiffness: 30, delay: 0.6 }}
             >
                 Je m'appelle Augustin, j'ai {age} ans et je suis Développeur Web Full Stack en freelance.
             </motion.p>
-            <motion.p
-                className="text-center mt-[2vh] lg:mt-[5vh] mx-3 text-xl lg:text-3xl"
-                initial={{ opacity: 0, x: "100vw" }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ type: "spring", stiffness: 30, delay: 0.6 }}
-            >
-                J'ai toujours été passionné par l'informatique et la découverte du développement web il y a quelques années m'a comblé :
-            </motion.p>
-            <motion.p
-                className="text-center mx-3 text-xl lg:text-3xl"
-                initial={{ opacity: 0, x: "100vw" }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ type: "spring", stiffness: 30, delay: 0.6 }}
-            >
-                Ça m'a permis d'allier créativité et logique dans mes projets.
-            </motion.p>
-            <motion.p
-                className="text-center mt-[2vh] lg:mt-[5vh] mx-3 text-xl lg:text-3xl"
-                initial={{ opacity: 0, x: "100vw" }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ type: "spring", stiffness: 30, delay: 0.8 }}
-            >
-                Je serais ravi de vous accompagner dans la réalisation de vos projets web,
-            </motion.p>
-            <motion.p
-                className="text-center mx-3 text-xl lg:text-3xl"
-                initial={{ opacity: 0, x: "100vw" }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ type: "spring", stiffness: 30, delay: 0.9 }}
-            >
-                Naviguez sur mon portfolio pour découvrir mon parcours, mes compétences et mes réalisations, ou pour me contacter.
-            </motion.p>
+            <div>
+                <motion.p
+                    className="text-center mx-3 text-xl md:text-2xl lg:text-3xl"
+                    initial={{ opacity: 0, x: "100vw" }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: "spring", stiffness: 30, delay: 0.7 }}
+                >
+                    J'ai toujours été passionné par l'informatique et la découverte du développement web il y a quelques années m'a comblé :
+                </motion.p>
+                <motion.p
+                    className="text-center mx-3 text-xl md:text-2xl lg:text-3xl"
+                    initial={{ opacity: 0, x: "100vw" }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: "spring", stiffness: 30, delay: 0.8 }}
+                >
+                    Ça m'a permis d'allier créativité et logique dans mes projets !
+                </motion.p>
+            </div>
+            <div>
+                <motion.p
+                    className="text-center mx-3 text-xl md:text-2xl lg:text-3xl"
+                    initial={{ opacity: 0, x: "100vw" }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: "spring", stiffness: 30, delay: 0.9 }}
+                >
+                    Je serais ravi de vous accompagner dans la réalisation de vos projets web.
+                </motion.p>
+                <motion.p
+                    className="text-center mx-3 text-xl md:text-2xl lg:text-3xl"
+                    initial={{ opacity: 0, x: "100vw" }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ type: "spring", stiffness: 30, delay: 1 }}
+                >
+                    Naviguez sur mon portfolio pour découvrir mon parcours, mes compétences et mes réalisations, ou pour me contacter.
+                </motion.p>
+            </div>
             <img 
-                className="w-20 lg:w-40 mt-[12vh] lg:mt-[20vh] animate-bounce dark:block hidden" 
+                className="w-20 md:w-30 lg:w-40 animate-bounce dark:block hidden" 
                 src="/white_arrow.svg" 
                 alt="Flèche blanche pointant vers le bas"
             />
             <img 
-                className="w-20 lg:w-40 mt-[25vh] lg:mt-[20vh] animate-bounce block dark:hidden" 
+                className="w-20 md:w-30 lg:w-40 animate-bounce block dark:hidden" 
                 src="/black_arrow.svg" 
                 alt="Flèche noire pointant vers le bas"
             />
